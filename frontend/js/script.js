a4981d5d6ead6e9626025e162cc1e07638164da3
Gautam pandit <gautampandit797@gmail.com>
2024-10-18 23:24:21 +0530

    Added Some Message

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,7 +1,7 @@
 /* ------============================typing animation=======================*/
 var typed = new Typed(".typing",{
     strings:["","Software Engineer", "FullStack developer","Frontend Developer","Backend Developer"],
-    typespeed:100,
+    typespeed:200,
     Backspeed:60,
     loop:true
 }) 
